132dfce1f9e70990bfaaee4e542876f71d1e1193
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-09-29 19:10:38 +0300

    29.09.2023

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 from item.models import Category, Item
 
@@ -13,3 +14,10 @@ def index(request):
 
 def contact(request):
     return render(request, "core/contact.html")
+
+def signup(request):
+    form = SignupForm()
+    
+    return render(request, 'core/signup.html', {
+        'form': form    
+    })
