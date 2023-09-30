07c77e6957363e590d92a663905f803296849b32
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-09-30 14:58:34 +0300

    30.09.2023

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,33 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
+    
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
+    
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
+    
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-2 px-2 rounded-xl focus:outline-none'    
+    }))
