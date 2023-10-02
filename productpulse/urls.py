e42f2c55153ae0256456af009ee1a5c9b420ef19
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-10-02 19:21:15 +0300

    02.10.2023

--- productpulse/urls.py
+++ productpulse/urls.py
@@ -20,8 +20,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("items/", include('item.urls')),
     path("", include('core.urls')),
-    
+    path("items/", include('item.urls')),
+    path("dashboard/", include('dashboard.urls')),
+    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
